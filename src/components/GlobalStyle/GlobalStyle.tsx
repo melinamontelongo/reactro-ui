bf5b41c4907304a51fb02ca7619efb93403dee36
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-09-25 20:23:39 -0300

    feat: fonts

--- src/components/GlobalStyle/GlobalStyle.tsx
+++ src/components/GlobalStyle/GlobalStyle.tsx
@@ -1,16 +1,27 @@
 import { createGlobalStyle } from "styled-components";
 
 import retroGamingFont from "./fonts/retro-gaming.ttf";
+import alkhemikalFont from "./fonts/alkhemikal.ttf";
+import veniceFont from "./fonts/venice-classic.ttf";
 
 const GlobalStyle = createGlobalStyle`
 @font-face {
     font-family: "retro-gaming";
     src: url(${retroGamingFont}) format('truetype');
-    font-weight: normal;
-    font-style: normal;
 }
-*, body {
-    font-family: "retro-gaming", Helvetica, Sans-Serif;
+@font-face {
+  font-family: "alkhemikal";
+  src: url(${alkhemikalFont}) format('truetype');
+}
+@font-face {
+  font-family: "venice";
+  src: url(${veniceFont}) format('truetype');
+}
+*{
+  font-family: "retro-gaming", Helvetica, Sans-Serif;
+}
+body {
+    background-color: ${(props) => props.theme.colors.base};
   }
 `
 
