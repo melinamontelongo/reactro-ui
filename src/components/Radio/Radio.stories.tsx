b99e0cbc9bf693cb1957ed8c967d2351559423f8
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-10-09 21:29:21 -0300

    feat: radio story

--- src/components/Radio/Radio.stories.tsx
+++ src/components/Radio/Radio.stories.tsx
@@ -13,13 +13,13 @@ export default meta;
 
 type Story = StoryObj<typeof Radio>;
 
-export const Radios: Story = {
+export const Group: Story = {
     render: () => <>
         <Fieldset legend="Radio variants">
-            <Radio id="default-radio"  name="default" label="Default"/>
-            <Radio id="primary-radio"  name="primary" label="Primary"  variant="primary"/>
-            <Radio id="secondary-radio" name="secondary" label="Secondary"  variant="secondary"/>
-            <Radio id="base-radio" name="base" label="Base"  variant="base"/>
+            <Radio id="default-radio"  name="variant" label="Default"/>
+            <Radio id="primary-radio"  name="variant" label="Primary"  variant="primary"/>
+            <Radio id="secondary-radio" name="variant" label="Secondary"  variant="secondary"/>
+            <Radio id="base-radio" name="variant" label="Base"  variant="base"/>
         </Fieldset>
     </>
 }
