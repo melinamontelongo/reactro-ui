17fc5c97be470602d7d24094be44f126fdee4f2c
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-10-11 21:57:09 -0300

    feat: text component receives text as children

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -7,7 +7,7 @@ const meta: Meta<typeof Text> = {
     title: "Text",
     tags: ["autodocs"],
     argTypes: {
-        text: { control: "text" },
+        children: { control: "text" },
         fontFamily: {
             options: ["retro", "venice", "bitcell", "alkhemikal", "edunline", "owre"],
             control: { type: "select" },
@@ -23,16 +23,16 @@ export const TextTest: Story = (args) => (
     <Text {...args} />
 )
 TextTest.args = {
-    text: "Test me!",
+    children: "Test me!",
 
 }
 export const TextVariants: Story = {
     render: () => <>
-        <Text text="My font family is 'retro', which is the default across the library." />
-        <Text text="My font family is 'venice'." fontFamily="venice" fontSize="2rem" />
-        <Text text="My font family is 'bitcell'." fontFamily="bitcell" fontSize="2rem" />
-        <Text text="My font family is 'alkhemikal'." fontFamily="alkhemikal" fontSize="2rem" />
-        <Text text="My font family is 'edunline'." fontFamily="edunline" fontSize="2rem" />
-        <Text text="My font family is 'owre'." fontFamily="owre" fontSize="2rem" />
+        <Text>My font family is 'retro', which is the default across the library.</Text>
+        <Text fontFamily="venice" fontSize="2rem">My font family is 'venice'.</Text>
+        <Text fontFamily="bitcell" fontSize="2rem" >My font family is 'bitcell'.</Text>
+        <Text fontFamily="alkhemikal" fontSize="2rem">My font family is 'alkhemikal'.</Text>
+        <Text fontFamily="edunline" fontSize="2rem">My font family is 'edunline'.</Text>
+        <Text fontFamily="owre" fontSize="2rem">My font family is 'owre'.</Text>
     </>
 }
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { FontFamily, TextTags } from "../ThemeProvider/ThemeProvider";
 
 export interface TextProps extends HTMLAttributes<HTMLHeadingElement | HTMLParagraphElement> {
-    text: string,
+    children: string,
     tag?: TextTags,
     fontSize?: string,
     fontFamily?: FontFamily,
@@ -13,10 +13,10 @@ const StyledText = styled.div<Pick<TextProps, "fontSize" | "fontFamily">>`
     font-size: ${(props) => props.fontSize};
     font-family: ${(props) => props.fontFamily};
 `
-const Text = ({ text, tag = "p", fontSize = "1rem", fontFamily = "retro" }: TextProps) => {
+const Text = ({ children, tag = "p", fontSize = "1rem", fontFamily = "retro" }: TextProps) => {
     return (
         <StyledText as={tag} fontSize={fontSize} fontFamily={fontFamily}>
-            {text}
+            {children}
         </StyledText>
     )
 }
