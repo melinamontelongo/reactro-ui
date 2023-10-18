394b0d2bf35e1095489596a3d22cca032b6c6ab6
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-10-18 17:39:00 -0300

    feat: flex container width and div props

--- src/components/FlexContainer/FlexContainer.tsx
+++ src/components/FlexContainer/FlexContainer.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { HTMLAttributes } from "react";
 import styled from "styled-components";
 
-interface FlexContainerProps {
+interface FlexContainerProps extends HTMLAttributes<HTMLDivElement> {
     children: React.ReactNode,
     direction?: "row" | "column",
     justifyContent?: "start" | "end" | "center" | "space-between" | "space-around" | "space-evenly" | "stretch" | "normal",
     alignItems?: "stretch" | "center" | "start" | "end" | "normal"
     gap?: string,
     wrap?: "wrap" | "no-wrap",
+    width?: string
 }
 interface StyledFlexContainerProps {
     $direction?: "row" | "column",
@@ -15,6 +16,7 @@ interface StyledFlexContainerProps {
     $alignItems?: "stretch" | "center" | "start" | "end" | "normal"
     $gap?: string,
     $wrap?: "wrap" | "no-wrap",
+    $width?: string
 }
 const StyledContainer = styled.div<StyledFlexContainerProps>`
     display: flex;
@@ -22,12 +24,13 @@ const StyledContainer = styled.div<StyledFlexContainerProps>`
     justify-content: ${(props) => props.$justifyContent};
     align-items: ${(props) => props.$alignItems};
     gap: ${(props) => props.$gap};
+    width: ${(props) => props.$width};
     flex-wrap: wrap;
 `
 
-const FlexContainer = ({ children, direction = "row", justifyContent = "start", alignItems = "start", gap = "0px", wrap="wrap" }: FlexContainerProps) => {
+const FlexContainer = ({ children, direction = "row", justifyContent = "start", alignItems = "start", gap = "0px", wrap="wrap", width = "fit-content", ...props }: FlexContainerProps) => {
     return (
-        <StyledContainer $direction={direction} $justifyContent={justifyContent} $alignItems={alignItems} $gap={gap} $wrap={wrap}>
+        <StyledContainer $direction={direction} $justifyContent={justifyContent} $alignItems={alignItems} $gap={gap} $wrap={wrap} $width={width} {...props}>
             {children}
         </StyledContainer>
     )
