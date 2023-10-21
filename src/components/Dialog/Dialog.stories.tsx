86f6be8e632cbd33728cf33d71c9040e89d91b00
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-10-21 17:48:23 -0300

    fix: dialog stories content props

--- src/components/Dialog/Dialog.stories.tsx
+++ src/components/Dialog/Dialog.stories.tsx
@@ -18,14 +18,14 @@ export const Default: Story = (args) => (
 );
 Default.args = {
   title: "Default dialog",
-  content: <>
+  dialogContent: <>
     <p>This is the content of a default dialog box with two default buttons passed as footer.</p>
   </>,
   width: "400px",
   height: "200px",
   footer: <div style={{ display: "flex", flexDirection: "row", justifyContent: "end", gap: "10px" }}>
-    <Button size={"lg"} variant="primary">OK</Button>
-    <Button size={"lg"} variant="primary">Cancel</Button>
+    <Button size={"lg"}>OK</Button>
+    <Button size={"lg"}>Cancel</Button>
   </div>,
 };
 
@@ -34,7 +34,7 @@ export const Primary: Story = (args) => (
 );
 Primary.args = {
   title: "Primary dialog",
-  content: <>
+  dialogContent: <>
     <p>This is the content of a primary dialog box with two primary buttons passed as footer.</p>
   </>,
   width: "400px",
@@ -51,15 +51,15 @@ export const Secondary: Story = (args) => (
 );
 Secondary.args = {
   title: "Secondary dialog",
-  content: <>
+  dialogContent: <>
     <p>This is the content of a secondary dialog box with two secondary buttons passed as footer.</p>
   </>,
   width: "400px",
   height: "200px",
   variant: "secondary",
   footer: <div style={{ display: "flex", flexDirection: "row", justifyContent: "end", gap: "10px" }}>
-    <Button size={"lg"} variant="primary">OK</Button>
-    <Button size={"lg"} variant="primary">Cancel</Button>
+    <Button size={"lg"} variant="secondary">OK</Button>
+    <Button size={"lg"} variant="secondary">Cancel</Button>
   </div>,
 };
 
@@ -68,15 +68,15 @@ export const Base: Story = (args) => (
 );
 Base.args = {
   title: "Base dialog",
-  content: <>
+  dialogContent: <>
     <p>This is the content of a base dialog box with two base buttons passed as footer.</p>
   </>,
   width: "400px",
   height: "200px",
   variant: "base",
   footer: <div style={{ display: "flex", flexDirection: "row", justifyContent: "end", gap: "10px" }}>
-    <Button size={"lg"} variant="primary">OK</Button>
-    <Button size={"lg"} variant="primary">Cancel</Button>
+    <Button size={"lg"} variant="base">OK</Button>
+    <Button size={"lg"} variant="base">Cancel</Button>
   </div>,
 };
 
@@ -85,7 +85,7 @@ export const Overflown: Story = (args) => (
 );
 Overflown.args = {
   title: "Overflown dialog",
-  content: <>
+  dialogContent: <>
     <p>This is the content of an overflown dialog box.</p>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit veritatis ipsa provident totam, dicta dolorem iure corporis expedita? Odio tenetur rem velit explicabo ea fuga et voluptas, rerum perferendis beatae?</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sit dolor vero iure accusamus illum qui voluptate, porro magnam, ab dolores eaque cum ducimus incidunt quas! Delectus magnam magni blanditiis.</p>
